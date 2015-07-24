0496f15b561fa13cb88578969c77e7e9133dfcfa
flo mac <flo@flo.com>
2015-07-24 14:22:48 +0200

    list view added

--- plateformeExecution/main.py
+++ plateformeExecution/main.py
@@ -10,6 +10,9 @@ class AglLayout(FloatLayout):
     """
     action_bar_title = "AGL"
 
+    def spec_on_select(self):
+        print "row_selected"
+
     def quit(self):
         AglApp().stop()
 
