c5f59e6fb8e99e55931c89d0d0846cb733275d2b
Alonso <aonn18@gmail.com>
2015-08-31 05:15:01 -0700

    Cleaning main.py

--- plateformeExecution/main.py
+++ plateformeExecution/main.py
@@ -1,15 +1,9 @@
 from kivy.app import App
-from kivy.factory import Factory
 from kivy.uix.floatlayout import FloatLayout
 
 import subprocess
 
-
-
 class AglLayout(FloatLayout):
-    """
-
-    """
     action_bar_title = "AGL"
 
     def quit(self):
@@ -21,14 +15,7 @@ class AglLayout(FloatLayout):
     def spec_on_select(self):
         print "row_selected"
 
-    def launch_notepad(self):
-        subprocess.Popen('C:\Windows\System32\notepad.exe')
-
-
 class AglApp(App):
-    """
-
-    """
     def build(self):
         self.root = AglLayout()
 
