4a83c6dbc97754d5a0f12fc59a4bb529a5881292
Alonso <aonn18@gmail.com>
2015-08-31 07:23:22 -0700

    program class for start program in a thread

--- plateformeExecution/main.py
+++ plateformeExecution/main.py
@@ -1,7 +1,9 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-
+import psutil
 import subprocess
+from program import Program
+
 
 class AglLayout(FloatLayout):
     action_bar_title = "AGL"
@@ -11,10 +13,22 @@ class AglLayout(FloatLayout):
         AglApp().stop()
         
     def launch_dia(self):
-        subprocess.call([self.dia_path])
-        
+        is_running = False
+        for p in psutil.process_iter():
+            try:
+                if p.name() == 'dia.exe':
+                    print "dia is already running"
+                    is_running = True
+            except psutil.error:
+                pass
+        if not is_running:
+            dia = Program(self.dia_path)
+            dia.start()
+            print "program {} started".format(self.dia_path)
+
     def spec_on_select(self):
         print "row_selected"
+
 
 class AglApp(App):
     def build(self):
