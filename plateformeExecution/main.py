090b763b07b215c6aafc023ec5b44659edaf7fcf
Alonso <aonn18@gmail.com>
2015-09-05 18:18:34 +0200

    some cleaning in main.py

--- plateformeExecution/main.py
+++ plateformeExecution/main.py
@@ -4,7 +4,8 @@ import os
 
 from kivy.app import App
 from kivy.event import EventDispatcher
-from kivy.properties import ListProperty, ObjectProperty, DictProperty, NumericProperty
+from kivy.properties import ListProperty, ObjectProperty, DictProperty, NumericProperty, StringProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
 from kivy.uix.floatlayout import FloatLayout
@@ -28,7 +29,7 @@ class AttributeContainer(object):
     __metaclass__ = Singleton
 
     current_project = None
-
+    # TEST : stub of the referentiel tree
     referentiel_tree = {'node_id': '1',
                         'children': [{'node_id': '1.1',
                                       'children': [{'node_id': '1.1.1',
@@ -82,21 +83,20 @@ class ProjectScreen(Screen):
         AttributeContainer().current_project = new_project
 
 
-class SpecificationListener(EventDispatcher):
+class TreeHandler(object):
     """
-    Handle changes from this project's specification directory
+        Stack of methods for operations on tree
     """
-    specifications = ListProperty([])
 
+    @classmethod
+    def populate_tree(cls, tree_view, parent, node):
+        if parent is None:
+            tree_node = tree_view.add_node(TreeViewLabel(text=node['node_id'], is_open=True))
+        else:
+            tree_node = tree_view.add_node(TreeViewLabel(text=node['node_id'], is_open=True), parent)
 
-def populate_tree(tree_view, parent, node):
-    if parent is None:
-        tree_node = tree_view.add_node(TreeViewLabel(text=node['node_id'], is_open=True))
-    else:
-        tree_node = tree_view.add_node(TreeViewLabel(text=node['node_id'], is_open=True), parent)
-
-    for child_node in node['children']:
-        populate_tree(tree_view, tree_node, child_node)
+        for child_node in node['children']:
+            TreeHandler.populate_tree(tree_view, tree_node, child_node)
 
 
 class MainScreen(Screen):
@@ -104,6 +104,7 @@ class MainScreen(Screen):
     dia_path = 'C:\\Program Files (x86)\\Dia\\bin\\dia.exe'
     # IDs in kv
     ref_tree_widget = ObjectProperty(None)
+    ref_preview_widget = ObjectProperty(None)
 
     def launch_dia(self):
         is_running = False
@@ -119,15 +120,14 @@ class MainScreen(Screen):
             dia.start()
             print "program {} started".format(self.dia_path)
 
-    def spec_on_select(self):
-        print "row_selected"
-
 
 class RefTreeWidget(FloatLayout):
     """
         Widget for the referentiel tree view
     """
+    # Properties attributes
     tree_changes = DictProperty(AttributeContainer().referentiel_tree)
+    # Regular attributes
     tree_view = TreeView()
 
     def __init__(self, **kwargs):
@@ -135,6 +135,10 @@ class RefTreeWidget(FloatLayout):
         self.update_tree()
 
     def change_ref(self):
+        """
+        [TEST] stub for changing the referentiel dict
+        :return:
+        """
         self.tree_changes = dict({'node_id': 'new 1',
                                   'children': [{'node_id': 'new 1.1',
                                                 'children': []
@@ -156,10 +160,16 @@ class RefTreeWidget(FloatLayout):
         self.tree_view = TreeView(root_options=dict(text=project_name),
                                   hide_root=False,
                                   indent_level=5)
-        populate_tree(self.tree_view, None, self.tree_changes)
+        TreeHandler.populate_tree(self.tree_view, None, self.tree_changes)
         self.add_widget(self.tree_view)
 
     def on_tree_changes(self, instance, value):
+        """
+        Trigger when self.tree_changes has changed
+        :param instance:
+        :param value:
+        :return: void
+        """
         print "referentiel tree has changed"
         self.update_tree()
 
