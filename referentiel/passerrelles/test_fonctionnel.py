e56c0334cf97acd756e2996f7e1b474c5d0a2a4f
Raphaël Fontenay <raphael.fontenay@gmail.com>
2015-09-11 08:51:48 +0200

    For the functional test, only the classe name is keeped because a functionnal test is composed of several method.

---
+++ referentiel/passerrelles/test_fonctionnel.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import tkFileDialog
+import plyj.parser as plyj
+import plyj.model as m
+
+
+class JavaParser():
+
+    def parse(self):
+        file_path_string = tkFileDialog.askopenfilename()
+        files = [file_path_string]
+
+        json = []
+
+        for fil in files:
+            parser = plyj.Parser()
+            tree = parser.parse_file(file(fil))
+            for type_decl in tree.type_declarations:
+                json.append(type_decl.name)
+            print json
+        return json
+
+if __name__ == '__main__':
+    jp = JavaParser()
+    JavaParser.parse(jp)
