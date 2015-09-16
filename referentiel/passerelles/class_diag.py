c730f2f4c199039b26daefe79c4196272b8f5426
Raphaël Fontenay <raphael.fontenay@gmail.com>
2015-09-16 12:51:03 +0200

    [classdiag] Now return file name(without extension), new return format is [[f1,c1,c2],[f2,c1,c2]]

--- referentiel/passerelles/class_diag.py
+++ referentiel/passerelles/class_diag.py
@@ -12,7 +12,7 @@ class DiagParser():
     def parse(self, files_list):
         """
 
-        :return: List of diagram names
+        :return: List of diagram names + file_name : [[f1,c1,c2],[f2,c1,c2,c3]
         """
         files = files_list
         for fil in files:
@@ -20,15 +20,18 @@ class DiagParser():
                 files.remove(fil)
 
         classes = []
-
+        result = []
         dia = "{http://www.lysator.liu.se/~alla/dia/}"
         path = "{0}layer/{0}object[@type='UML - Class']/{0}attribute[@name='name']/{0}string".format(dia)
         for fil in files:
             tree = etree.parse(fil)
+            classes.append(os.path.basename(fil)[:4])
             for cla in tree.findall(path):
                 classes.append(cla.text[1:len(cla.text)-1])
-        print classes
-        return classes
+            result.append(classes)
+            classes = []
+        print result
+        return result
         # usecases contains the name of the usecase that will be given to the json
 
 if __name__ == '__main__':
